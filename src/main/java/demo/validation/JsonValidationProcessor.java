c02e3e13759d25225da099c68c88002cc17956f6
ivan <ivg.ivanov@gmail.com>
2018-07-03 00:38:01 +0300

    Json validation processor improved.

--- src/main/java/demo/validation/JsonValidationProcessor.java
+++ src/main/java/demo/validation/JsonValidationProcessor.java
@@ -6,8 +6,11 @@ import org.apache.camel.Processor;
 import org.apache.camel.component.jsonvalidator.JsonValidationException;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 /**
  * Created because the DefaultJsonValidatorErrorHandler does not log errors properly.
+ * Here we process the exception and display correct error message.
  */
 @Component
 public class JsonValidationProcessor implements Processor {
@@ -22,9 +25,14 @@ public class JsonValidationProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         JsonValidationException exception = (JsonValidationException) exchange.getProperty(Exchange.EXCEPTION_CAUGHT);
+        Set<ValidationMessage> errors = exception.getErrors();
         String errorText = "";
-        for (ValidationMessage msg : exception.getErrors()) {
-            errorText += msg.getMessage() + ";";
+        if (errors == null) {
+            errorText = exception.toString();
+        } else {
+            for (ValidationMessage msg : errors) {
+                errorText += msg.getMessage() + ";";
+            }
         }
 
         exchange.getOut().setBody(String.format("{\"error\": \"%s\"}", errorText));
